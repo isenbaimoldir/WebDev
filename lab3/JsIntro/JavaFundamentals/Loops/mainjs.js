f00666be53355e10f1463f74c649c00c5de03fd0
senator17 <amoka8707@gmail.com>
2025-02-19 08:07:17 +0500

    d

---
+++ lab3/JsIntro/JavaFundamentals/Loops/mainjs.js
@@ -0,0 +1,13 @@
+
+for (let i = 0; i < 5; i++) alert( i );
+for (let i = 0; i < 5; ++i) alert( i );
+
+for(let i = 2; i <= 10; i +=2){
+    alert(i);
+}
+
+let i = 0;
+while(i < 3){
+    alert( `number ${i}!` );
+    i ++;
+}
